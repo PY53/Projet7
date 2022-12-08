f4946c11ee9815618a6f7fb1daffa176efb3b174
PY53 <py.prodhomme@gmail.com>
2022-12-08 10:07:20 +0100

    dashboard mise à jour,import de st_aggrid, plotly + API mise à jour, import de shap, time

--- app.py
+++ app.py
@@ -9,6 +9,7 @@
 # numpy  1.21.6
 # requests 2.28.1
 # scikit-learn==1.0.2 => sklearn est utilisé à travers la fonction joblib
+# shape ...
 
 # python app.py
 
@@ -17,27 +18,61 @@ import joblib
 import pickle
 import numpy as np
 from requests.models import Response
+import shap
+import time
 
 app = Flask(__name__)
-
-loaded_model = joblib.load('model_lgbm.joblib')
-print("========== model loaded ==============")
 
 @app.route("/")
 def root():
     return render_template("index.html")
 
+# @app.route("/loading_model", methods=['GET'])
+# def load_model():
+#     print("========== loading model ==============")
+#     loaded_model = joblib.load('model_lgbm.joblib')
+#     print("========== model loaded ==============")
+    
+#     # on affiche le résultat sur la page index.html avec la méthode GET
+#     msg = "========== model loaded =============="
+#     return render_template("index.html", text_from_app=msg)
+
+
+# @app.route("/init", methods=['GET'])
+# def init():  
+#     df = pd.read_csv('dataset/data_for_mini_model.csv', nrows=6)
+#     msg = "========== init data =============="
+#     return render_template("index.html", text= msg)
+#     print("========== init data ==============")
+#     response = app.response_class(
+#         response=json.dumps([df]),
+#         status=200,
+#         mimetype='application/json'
+#     )
+#     return response
+    # return make_response(jsonify(df), 200, mimetype='application/json')
+
 @app.route("/predict", methods=['GET', 'POST'])
 def make_prediction():
 
-    proba = np.Nan
+    print("========== loading model ==============")
+    loaded_model = joblib.load('model_lgbm.joblib')
+    print("========== model loaded ==============")
+    
+    # # on affiche le résultat sur la page index.html avec la méthode GET
+    # msg = "========== model loaded =============="
+    # return render_template("index.html", text_from_app=msg)
+    
+    
+    proba = np.nan
     print("============= request method : {}=============".format(request.method))
 
     if request.method=='POST':
         # Pour lancer une requête POST depuis l'invite de commande, taper dans l'invite de commande :
         # curl http://127.0.0.1:5000/predict -H "Content-Type: application/json" -d "{\"data\": [[1, 2, 3, 4, 5, 6, 7, 8]]}"
         data = request.get_json()
-        print("============= data: {}=============".format(data['data']))
+        # print("============= data: {}=============".format(data['data']))
+        print("============= data shape: {}=============".format(np.shape(data['data'])))
         # print("============= data : {}=============".format(request.form['data']))
         X = np.array([float(var) for var in data['data'][0]])
     elif request.method=='GET':
@@ -48,16 +83,29 @@ def make_prediction():
         data = request.args
         X = np.array([float(var) for var in data.values()])
     
-    print("============= X : {}=============".format(X))
+    print("X shape = {}".format(X.shape))
+    if X.reshape(1, -1).shape != (1,732):
+        raise ValueError("X has not (1,732) size")
+    
+    # print("============= X : {}=============".format(X))
     proba = loaded_model.predict_proba(X.reshape(1, -1))[0][1]
-    # print("============= prediction : {}=============".format(proba))  
+    # print("============= prediction : {}=============".format(proba))
+    
+    
     proba = proba.round(2)
-    print("============= prediction : {}=============".format(proba))  
-    
-    # On renvoir une réponse à l'aide de la classe response_class de flask
-    
+    print("============= prediction : {}=============".format([proba]))  
+
+    shap_wanted=False
+    if shap_wanted:
+        explainer = shap.TreeExplainer(loaded_model)
+        start = time.time()
+        shap_values = explainer.shap_values(df0[features_names].values)
+        time_shapvalues = time.time()-start
+        print("shapvalues computing time : {}" .format(time_shapvalues))
+      
+    # On renvoie une réponse à l'aide de la classe response_class de flask
     response = app.response_class(
-        response=json.dumps(list(proba)),
+        response=json.dumps([proba]),
         status=200,
         mimetype='application/json'
     )
--- dashboard.py
+++ dashboard.py
@@ -1,18 +1,57 @@
 import pandas as pd
 import streamlit as st
 import requests
+import numpy as np
+from st_aggrid import AgGrid, GridOptionsBuilder
+from st_aggrid.shared import GridUpdateMode
+import plotly.figure_factory as ff
+import plotly.express as px
 
 # requests 2.28.1
 # streamlit 1.14.0
+# streamlit-aggrid 0.3.2 (installer avec Pip)
+# plotly 5.11.0 (conda install -c plotly plotly=5.11.0)
 # pandas 1.3.5
+
 
 # streamlit run dashboard.py
 
+# create an interactive table
+def aggrid_interactive_table(df: pd.DataFrame):
+    """Creates an st-aggrid interactive table based on a dataframe.
+
+    Args:
+        df (pd.DataFrame]): Source dataframe
+
+    Returns:
+        dict: The selected row
+    """
+    options = GridOptionsBuilder.from_dataframe(
+        df, enableRowGroup=True, enableValue=True, enablePivot=True
+    )
+
+    options.configure_side_bar()
+
+    options.configure_selection("single")
+    selection = AgGrid(
+        df,
+        enable_enterprise_modules=True,
+        gridOptions=options.build(),
+        theme="light",
+        update_mode=GridUpdateMode.MODEL_CHANGED,
+        allow_unsafe_jscode=True,
+    )
+
+    return selection
+
+
 def request_prediction(model_uri, data):
     headers = {"Content-Type": "application/json"}
-
+  
+    print("============== data.shape :", np.shape(data))
+    print("===================data type = ", type(data))
     data_json = {'data': data}
-    print("===================data_json = {} ".format(data_json))
+    print("===================data_json type = ", type(data_json))
     response = requests.request(method='POST', 
                                 headers=headers, url=model_uri, json=data_json)
 
@@ -30,7 +69,10 @@ def request_prediction(model_uri, data):
 def main():
     # FLASK_URI = 'http://projet7a-py0153.pythonanywhere.com/predict'
     FLASK_URI = 'http://127.0.0.1:5000/predict'
-
+    # LOAD_MODEL_URI = 'http://127.0.0.1:5000/loading_model'
+    # response = requests.request(method='GET',  url=LOAD_MODEL_URI)
+    # print("===================model loaded =====")
+    
     api_choice = st.sidebar.selectbox(
         'Quelle API souhaitez vous utiliser',
         ['Flask', 'Cortex', 'Ray Serve'])
@@ -38,54 +80,109 @@ def main():
     st.title('Failure risk estimation for loan delivery')
 
     print("Lecture de la base de données")
-    df = pd.read_csv('dataset/featureengineeringdata.csv', nrow=5)
+    df = pd.read_csv('dataset/data_for_mini_model.csv') # nrows=6)
     
-    revenu_med = st.number_input('Revenu médian dans le secteur (en 10K de dollars)',
-                                 min_value=0., value=3.87, step=1.)
-
-    age_med = st.number_input('Âge médian des maisons dans le secteur',
-                              min_value=0., value=28., step=1.)
-
-    nb_piece_med = st.number_input('Nombre moyen de pièces',
-                                   min_value=0., value=5., step=1.)
-
-    nb_chambre_moy = st.number_input('Nombre moyen de chambres',
-                                     min_value=0., value=1., step=1.)
-
-    taille_pop = st.number_input('Taille de la population dans le secteur',
-                                 min_value=0, value=1425, step=100)
-
-    occupation_moy = st.number_input('Occupation moyenne de la maison (en nombre d\'habitants)',
-                                     min_value=0., value=3., step=1.)
-
-    latitude = st.number_input('Latitude du secteur',
-                               value=35., step=1.)
-
-    longitude = st.number_input('Longitude du secteur',
-                                value=-119., step=1.)
-
-    sample_index = st.number_input("Index du client",
-                                min_value=0, value=5, step=1)
+    st.write("Data shape = {} ".format(df.shape))
     
+    options = st.multiselect(
+    'Quelles features souhaitez vous filtrer ?',
+    df.columns, "CNT_CHILDREN")
+
+    st.write('You selected:', options[0])
+   
+    # filter_btn = st.button('Filtrer')
+    
+    if options:
+
+        # Histogram data
+        x = df[options[0]].values
+
+        # Group data together
+        hist_data = [x]
+        # hist_data = [x1, x2, x3]
+
+        group_labels = ['Group 1']
+        # group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+#         # Create distplot with custom bin_size
+        # fig = ff.create_distplot(hist_data, group_labels, show_curve =False) # bin_size=[.1, .25, .5]   
+#         # Plot!
+        # st.plotly_chart(fig, use_container_width=True)
+        fig = px.histogram(x=df[options[0]].values)
+        st.plotly_chart(fig) # 
+    
+        # x_min = np.round(x.min(), 2)
+        # x_max = np.round(x.max(), 2)
+        x_min = float(x.min())
+        x_max = float(x.max())
+        
+        # values = filter_slider(x_min, x_max)
+        # def filter_slider(x_min, x_max):
+        values = st.slider('Select a range of values',
+                                x_min, x_max, (x_min, x_max)) 
+        st.write('values:', values)
+        
+        # réduction du nombre de lignes, au-dessus de 200 MB 
+        # (pandas) utilise environ 4 fois la taille du tableau en mémoire 
+        number_of_rows = ((df[options[0]]>=values[0]) & (df[options[0]]<=values[1])).sum()
+        st.write('Number of rows:', number_of_rows )  
+        
+        # proceed_btn = st.button('Proceed')
+        
+        selection=[]
+        if number_of_rows<1000 :  # En théorie 5000 ça passe mais ça ramme sur le PC
+            
+            selection = aggrid_interactive_table(df = df[(df[options[0]]>=values[0]) & (df[options[0]]<=values[1])])
+            st.write("clés de selection :", selection.keys())
+            # selection.data :  données d'entrées = df 
+            # selection.selected_rows :  ligne(s) sélectionnée(s) par l'utilisateur (mais ne contient pas l'index du df)
+            # selection.column_state : état des colonnes de la/les ligne(s) sélectionnée(s) par l'utilisateur
+
+            # TO DEBUG
+            if selection.column_state :
+                # transforme l'objet selection.selected_rows en DataFrame pour en extraire l'indice.
+                selected_row_index = pd.DataFrame(selection.selected_rows).rowIndex.values[0]
+            
+                st.write("Vous avez selectionné l'index:", selected_row_index)  # ce n'est pas l'index de df
+                data = selection.selected_rows[0]
+                st.session_state.change_index=False
+    
+    # Pour permettre de sélectionner un index autre que celui filtré
+    if 'change_index' not in st.session_state:
+        st.session_state.change_index=False
+    def data_from_index():
+        st.session_state.change_index=True
+    sample_index = st.number_input("Si vous souhaitez sélectionner un autre client, indiquez son index sinon appuyez directement sur 'Prédire'",
+                                                  min_value=-1, max_value=len(df),  step=1, on_change=data_from_index)
+    st.write("change_index", st.session_state.change_index)
+    
+    if st.session_state.change_index :
+        data = [list(df.iloc[sample_index].values)]
+
     predict_btn = st.button('Prédire')
+       
     if predict_btn:
-        data = [df.iloc[sample_index].values]
+        
+        # data = [list(df.iloc[sample_index].values)]
+
         # data = [[revenu_med, age_med, nb_piece_med, nb_chambre_moy,
         #          taille_pop, occupation_moy, latitude, longitude]]
-        print("===================data = {} ".format(data))
-        print("===================data = {} ".format(np.shape(data)))
+        # print("===================data = {} ".format(data))
+        st.write("Data shape = {} ".format(np.shape(data)))
         # [[3.87, 28.0, 5.0, 1.0, 1425, 3.0, 35.0, -119.0]]
         
         pred = None
 
-        if api_choice == 'Flask':
-            pred = request_prediction(FLASK_URI, data)
-        elif api_choice == 'Cortex':
-            pred = request_prediction(CORTEX_URI, data)[0] * 100000
-        elif api_choice == 'Ray Serve':
-            pred = request_prediction(RAY_SERVE_URI, data)[0] * 100000
+        # if api_choice == 'Flask':
+        pred = request_prediction(FLASK_URI, data)[0]
+        # elif api_choice == 'Cortex':
+            # pred = request_prediction(CORTEX_URI, data)[0] * 100000
+        # elif api_choice == 'Ray Serve':
+            # pred = request_prediction(RAY_SERVE_URI, data)[0] * 100000
         st.write(
             "La proba d'une défaillance du client est de {:.2f}".format(pred))
 
 if __name__ == '__main__':
     main()
+
+
